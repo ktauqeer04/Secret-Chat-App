04984dd7dd08c44470ff67e0d104ede6191872a5
Mohammad Tauqeer Kondkari <mohammedtauqeer4oct@gmail.com>
2025-03-24 01:16:32 +0530

    two way communication

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,17 @@ async function initServer() {
         cors: { origin: '*', methods: ['GET', 'POST'] },
     })
 
+    io.on('connection', (socket) => {
+        console.log(socket.id);
+
+        socket.on('temp', (data) => {
+            console.log(data); 
+            socket.broadcast.emit('send-message', data);
+        })
+        socket.emit("temp", "this is a temp message");
+        socket.on('disconnect', () => console.log('user disconnected'));
+    })
+
     app.use(express.json());
     app.use(cors());
 
