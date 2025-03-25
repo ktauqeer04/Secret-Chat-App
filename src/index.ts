22f3dcf0d26dfdb9e243270f3d39876dfcadf092
Mohammad Tauqeer Kondkari <mohammedtauqeer4oct@gmail.com>
2025-03-26 01:21:29 +0530

    PUB-SUB architecture completeed

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Server } from "socket.io";
 import { createServer } from "http";
 import cors from "cors";
 import { PORT } from "./config/server-config";
+import { Pub, Sub } from "./config/redis-config";
 
 
 
@@ -14,15 +15,28 @@ async function initServer() {
         cors: { origin: '*', methods: ['GET', 'POST'] },
     })
 
+    Sub.subscribe('MESSAGE');
+
     io.on('connection', (socket) => {
         console.log(socket.id);
 
-        socket.on('temp', (data) => {
+        socket.on('temp', async (data) => {
+
             console.log(data); 
+
+            await Pub.publish('MESSAGE', JSON.stringify(data));
+
             socket.broadcast.emit('send-message', data);
-        })
-        socket.emit("temp", "this is a temp message");
+        })  
+        // socket.emit("temp", "this is a temp message");
         socket.on('disconnect', () => console.log('user disconnected'));
+    })
+
+    Sub.on('message', async (channel, message) => {
+        if(channel === 'MESSAGE'){
+            io.emit("message", message);
+            console.log("message from terminal", message);
+        }
     })
 
     app.use(express.json());
